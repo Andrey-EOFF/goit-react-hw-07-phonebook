1f91d14a7be1774792527bc460a1651f0516f83f
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-06-19 12:17:40 +0300

    Summary

--- src/redux/contacts/contactsOperation.js
+++ src/redux/contacts/contactsOperation.js
@@ -1,15 +1,23 @@
-import * as contactsActions from './contactsAction';
 import * as contactsApiAll from 'api/contactsApi';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchContacts = () => async dispatch => {
-  dispatch(contactsActions.fetchContactsRequest());
-
-    try {
-      
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContactsAll',
+  async () => {
     const contacts = await contactsApiAll.fetchContactsAPI();
-        dispatch(contactsActions.fetchContactsSuccess(contacts));
-        
-    } catch (error) {
-        dispatch(contactsActions.fetchContactsError());
+    return contacts;
   }
-};
+);
+
+// export const fetchContacts = () => async dispatch => {
+//   dispatch(contactsActions.fetchContactsRequest());
+
+//     try {
+
+//     const contacts = await contactsApiAll.fetchContactsAPI();
+//         dispatch(contactsActions.fetchContactsSuccess(contacts));
+
+//     } catch (error) {
+//         dispatch(contactsActions.fetchContactsError());
+//   }
+// };
